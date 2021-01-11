b273e6ec8d0bbaf7b7c33a7871b6555edd2c44e8
Jack Mkimbo <nickihll@gmail.com>
2021-01-11 10:13:14 +0300

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,93 @@
+require(`dotenv`).config({
+  path: `.env`,
+});
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: `Opinionated Kenyan`,
+    siteTitleAlt: `OK Blog - Opinionated Kenyan Blog`,
+    siteHeadline: `OK Blog - Opinions of a Kenyan Youth`,
+    siteUrl: `https://minimal-blog.lekoarts.de`,
+    siteDescription: `Opinionated Kenyan is a blogsite touching on current affairs in the country for raising awareness.`,
+    siteLanguage: `en`,
+    siteImage: `/banner.jpg`,
+    author: `jack2dwrld`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      // See the theme's README for all available options
+      options: {
+        navigation: [
+          {
+            title: `Blog`,
+            slug: `/blog`,
+          },
+          {
+            title: `About`,
+            slug: `/about`,
+          },
+        ],
+        externalLinks: [
+          {
+            name: `Twitter`,
+            url: `https://twitter.com/lekoarts_de`,
+          },
+          {
+            name: `Instagram`,
+            url: `https://www.instagram.com/lekoarts.de/`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-disqus`,
+      options: {
+        shortname: `okblog-1`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `OK Blog - Opinionated Kenyan Blog`,
+        short_name: `OK Blog`,
+        description: `Opinionated Kenyan is a blogsite touching on current affairs in the country for raising awareness.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean),
+};
